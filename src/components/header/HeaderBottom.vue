5f79d3bf41130c1ae4b1fc0100176088aa798389
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-13 20:46:55 -0400

    [added] initial header

---
+++ src/components/header/HeaderBottom.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <ul>
+      <a href="/">
+        <li>Home</li>
+      </a>
+      <a href>
+        <li>About</li>
+      </a>
+      <a href>
+        <li>Services</li>
+      </a>
+      <a href>
+        <li>Our Team</li>
+      </a>
+      <a href>
+        <li>Jobs</li>
+      </a>
+      <a href>
+        <li>Contact</li>
+      </a>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderBottom"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/scss/variables";
+@import "../../styles/scss/placeholders";
+nav {
+  width: 100%;
+  position: fixed;
+  top: 82px;
+  height: calc(18px + 2rem);
+  background-color: $blue;
+  ul {
+    @extend %container;
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0 auto;
+    a {
+      text-decoration: none;
+      color: $white;
+      font-weight: bold;
+      &:hover {
+        background-color: $red;
+      }
+      li {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        height: 50px;
+        padding: 0 20px;
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/header/HeaderTop.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="header-top">
+    <a href="/">
+      <img
+        src="http://expressrepairservice.com/Content/img/express-service-logo.png"
+        alt="JB Long and Son Body Shop Logo"
+      >
+    </a>
+    <div>
+      <div class="phone">
+        <font-awesome-icon icon="phone"/>
+        <div>
+          <span class="message">Call Us Now</span>
+          <span>(502) 695-4477</span>
+        </div>
+      </div>
+      <div class="hours-of-operation">
+        <font-awesome-icon icon="clock"/>
+        <div>
+          <span class="message">Hours of Operations</span>
+          <span>Mon - Fri 8:00am - 5:00pm</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderTop"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/scss/variables";
+@import "../../styles/scss/placeholders";
+.header-top {
+  @extend %container;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  padding: 1rem;
+  background-color: $white;
+  img {
+    height: 50px;
+  }
+  div {
+    display: flex;
+    .phone,
+    .hours-of-operation {
+      display: flex;
+      align-items: center;
+      padding: 0 1rem;
+      svg {
+        padding-right: 1rem;
+        width: 25px;
+        height: auto;
+      }
+      div {
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        span.message {
+          font-weight: bold;
+        }
+      }
+    }
+    .phone {
+      border-right: 2px solid $red;
+    }
+  }
+}
+</style>
