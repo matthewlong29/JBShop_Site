e63a794cd8e7f6c848711b330febf72ed890ca60
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-14 11:54:37 -0400

    [added] google fonts

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,20 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Open+Sans|Roboto");
+@import "/styles/scss/variables";
+
 body {
   margin: 0;
   margin-top: calc(82px + 50px); // top header + bottom header height
   min-height: 5000px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: $titleFont;
+}
+
+p, span, a, {
+  font-family: $bodyFont;
 }
 </style>
