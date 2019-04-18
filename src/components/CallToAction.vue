b654157c38be438a98b4eaaa642428b631307ca9
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-18 17:49:44 -0400

    [added] call to action component

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="call-to-action">
+    <div>
+      <h2>{{message}}</h2>
+      <a :href="link">{{linkMessage}}</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CallToAction",
+  props: ["message", "link", "linkMessage"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/scss/variables";
+@import "../styles/scss/placeholders";
+
+section.call-to-action {
+  width: 100vw;
+  background-color: lighten($color: $red, $amount: 1);
+  padding: 50px 0;
+  div {
+    @extend %container;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+    padding: 15px;
+    border: 1px solid transparentize($color: $white, $amount: 0.5);
+    background-color: darken($color: $red, $amount: 5);
+    color: $white;
+    h2 {
+      font-weight: normal;
+    }
+    a {
+      color: $white;
+      text-decoration: none;
+      background-color: darken($color: $red, $amount: 20);
+      padding: 10px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,25 @@
   <main>
     <MainSlider></MainSlider>
     <Services></Services>
+    <CallToAction
+      message="Walk-ins are always welcome, or call now to book an appointment!"
+      link="tel:5026954477"
+      linkMessage="(502) 695-4477"
+    ></CallToAction>
   </main>
 </template>
 
 <script>
 import MainSlider from "@/components/MainSlider";
 import Services from "@/components/services/Services";
+import CallToAction from "@/components/CallToAction";
 
 export default {
   name: "home",
   components: {
     MainSlider,
-    Services
+    Services,
+    CallToAction
   }
 };
 </script>
