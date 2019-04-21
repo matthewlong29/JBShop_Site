8d7224c943bbbfeca8d1068550709ceb2b63630d
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-21 12:59:25 -0400

    [added] google maps component

---
+++ src/components/GoogleMaps.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <gmap-map :center="center" :zoom="12" style="width:100%;  height: 400px;">
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+  data() {
+    return {
+      center: { lat: 38.193002, lng: -84.827381 },
+      markers: [],
+      places: [],
+      currentPlace: null
+    };
+  },
+
+  mounted() {
+    this.geolocate();
+  },
+
+  methods: {
+    // receives a place object via the autocomplete component
+    setPlace(place) {
+      this.currentPlace = place;
+    },
+    addMarker() {
+      if (this.currentPlace) {
+        const marker = {
+          lat: this.currentPlace.geometry.location.lat(),
+          lng: this.currentPlace.geometry.location.lng()
+        };
+        this.markers.push({ position: marker });
+        this.places.push(this.currentPlace);
+        this.center = marker;
+        this.currentPlace = null;
+      }
+    },
+    geolocate: function() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/scss/variables";
+@import "../styles/scss/placeholders";
+
+// Always set the map height explicitly to define the size of the div element that contains the map.
+#map {
+  height: 100%;
+}
+div {
+  border-top: 2px solid $red;
+}
+</style>
