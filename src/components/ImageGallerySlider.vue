40945523a84cc4beabe8fb46a59dbd477b2f109a
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-21 13:59:01 -0400

    [added] animations and captions

--- src/components/ImageGallerySlider.vue
+++ src/components/ImageGallerySlider.vue
@@ -12,7 +12,18 @@
       </div>
     </div>
     <div class="images-container" :style="{marginLeft: marginVal + 'px'}">
-      <img v-for="image in images" :key="image.id" :src="image">
+      <div
+        class="image"
+        v-for="image in images"
+        :key="image.id"
+        :style="{backgroundImage: `url(${image.url})`}"
+      >
+        <div class="overlay-top"></div>
+        <div class="overlay-bottom">
+          <h3>{{image.category}}</h3>
+          <p>{{image.message}}</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -23,21 +34,81 @@ export default {
   data() {
     return {
       images: [
-        "/images/stock/1.jpg",
-        "/images/stock/2.jpg",
-        "/images/stock/3.jpg",
-        "/images/stock/4.jpg",
-        "/images/stock/5.jpg",
-        "/images/stock/6.jpg",
-        "/images/stock/7.jpg",
-        "/images/stock/8.jpg",
-        "/images/stock/9.jpg",
-        "/images/stock/10.jpg",
-        "/images/stock/11.jpg",
-        "/images/stock/12.jpg",
-        "/images/stock/13.jpg",
-        "/images/stock/14.jpg",
-        "/images/stock/15.jpg"
+        {
+          url: "/images/stock/1.jpg",
+          category: "Body Work",
+          message: "im some message for image 1"
+        },
+        {
+          url: "/images/stock/2.jpg",
+          category: "Quality Control",
+          message: "im some message for image 2"
+        },
+        {
+          url: "/images/stock/3.jpg",
+          category: "Body Work",
+          message: "im some message for image 3"
+        },
+        {
+          url: "/images/stock/4.jpg",
+          category: "Body Work",
+          message: "im some message for image 4"
+        },
+        {
+          url: "/images/stock/5.jpg",
+          category: "Body Work",
+          message: "im some message for image 5"
+        },
+        {
+          url: "/images/stock/6.jpg",
+          category: "Paint",
+          message: "im some message for image 6"
+        },
+        {
+          url: "/images/stock/7.jpg",
+          category: "Quality Control",
+          message: "im some message for image 7"
+        },
+        {
+          url: "/images/stock/8.jpg",
+          category: "Quality Control",
+          message: "im some message for image 8"
+        },
+        {
+          url: "/images/stock/9.jpg",
+          category: "Quality Control",
+          message: "im some message for image 9"
+        },
+        {
+          url: "/images/stock/10.jpg",
+          category: "Quality Control",
+          message: "im some message for image 10"
+        },
+        {
+          url: "/images/stock/11.jpg",
+          category: "Quality Control",
+          message: "im some message for image 11"
+        },
+        {
+          url: "/images/stock/12.jpg",
+          category: "Body Work",
+          message: "im some message for image 12"
+        },
+        {
+          url: "/images/stock/13.jpg",
+          category: "Quality Control",
+          message: "im some message for image 13"
+        },
+        {
+          url: "/images/stock/14.jpg",
+          category: "Quality Control",
+          message: "im some message for image 14"
+        },
+        {
+          url: "/images/stock/15.jpg",
+          category: "Quality Control",
+          message: "im some message for image 15"
+        }
       ],
       marginVal: -30
     };
@@ -72,6 +143,17 @@ export default {
 @import "../styles/scss/variables";
 @import "../styles/scss/placeholders";
 
+@keyframes slide-up-fade-in {
+  0% {
+    opacity: 0;
+    transform: translate(0px, 40px);
+  }
+  100% {
+    opacity: 1;
+    transform: translate(0px, 0px);
+  }
+}
+
 section.image-gallery {
   overflow: hidden;
   margin: 5rem auto;
@@ -91,11 +173,54 @@ section.image-gallery {
     display: flex;
     width: 100%;
     transition: margin-left ease-in-out 0.25s;
-    img {
-      height: 100%;
-      width: 296px;
+    div.image {
+      height: 200px;
+      min-width: 296px;
       margin: 10px 0 10px 10px;
-      border: 2px solid transparentize($color: $red, $amount: 0.5);
+      background-size: cover;
+      background-position: center;
+      border: 2px solid lighten($color: $red, $amount: 5);
+      position: relative;
+      .overlay-top {
+        position: absolute;
+        background-color: transparentize($color: $gray, $amount: 0.25);
+        width: 100%;
+        height: 0%;
+        transition: height ease-in-out 0.25s;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .overlay-bottom {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        position: absolute;
+        width: 100%;
+        text-align: center;
+        visibility: hidden;
+        h3 {
+          color: $red;
+        }
+        p {
+          color: $white;
+          padding-bottom: 1rem;
+        }
+      }
+      &:hover {
+        .overlay-top {
+          height: 100%;
+        }
+        .overlay-bottom {
+          height: 100%;
+          visibility: visible;
+          h3,
+          p {
+            margin: 0;
+            animation: slide-up-fade-in ease 0.25s;
+          }
+        }
+      }
     }
   }
 }
