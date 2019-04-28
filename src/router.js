4c06e709487b68b8ee794ce9637bd99957a2d090
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-04-27 20:47:20 -0400

    [updated] router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
     }
-  ]
-})
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from);
+    if ("scrollRestoration" in history) {
+      history.scrollRestoration = "manual";
+    }
+    if (to.hash) {
+      return {
+        selector: to.hash,
+        offset: { x: 0, y: 172 }
+      };
+    }
+  }
+});
