c33df80772fc1deecf27a9f142513bce6bdeab8d
Matthew Long <long.matthew29@gmail.com>
2019-06-19 19:10:08 -0400

    [added] new form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,38 @@
     <CallToAction id="call-to-action"></CallToAction>
     <MeetUs id="team"></MeetUs>
     <NowHiring id="jobs"></NowHiring>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name">
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email">
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     <ImageGallerySlider id="image-gallery"></ImageGallerySlider>
     <Contact id="contact"></Contact>
     <!-- <GoogleMaps id="google-maps"></GoogleMaps> -->
