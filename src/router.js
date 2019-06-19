cbdab563303fd94a60aa6e57723b61b384c13f91
Matthew Long <long.matthew29@gmail.com>
2019-06-19 19:51:09 -0400

    [added] form for netlify testing

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Apply from "./views/Apply.vue";
+import SubmissionFail from "./views/SubmissionFail.vue";
+import SubmissionSuccess from "./views/SubmissionSuccess.vue";
 
 Vue.use(Router);
 
@@ -18,6 +20,16 @@ export default new Router({
       name: "apply",
       component: Apply
     },
+    {
+      path: '/thanks',
+      name: 'success',
+      component: SubmissionSuccess
+    },
+    {
+      path: '/404',
+      name: 'fail',
+      component: SubmissionFail
+    }
   ],
   scrollBehavior(to, from, savedPosition) {
     console.log(to, from);
